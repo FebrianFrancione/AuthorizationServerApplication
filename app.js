b952393b9005e132804cfb06dbbb8656015aa34b
LettuceFoot <febrian.francione@gmail.com>
2021-01-14 11:08:32 -0500

    added node.js and dependencies

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ console.log("Hello world")
 
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
+
+connectDB()
 
 const app = express()
 
