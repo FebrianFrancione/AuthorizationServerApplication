03e3ebb9969b64f80647a86479fbcd1128db26c3
LettuceFoot <febrian.francione@gmail.com>
2021-01-15 15:54:42 -0500

    added middleware and login works, with booing if not auth'd

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 //@desc Auth with google
 //@route GET /auth/google
-router.get('/google',passport.authenticate('google', {scope: ['profile'] }))
+router.get('/google', passport.authenticate('google', {scope: ['profile'] }))
 
 
 //@desc Google auth callback
@@ -18,5 +18,12 @@ router.get(
     }
 )
 
+// @desc Logout User
+//@route /auth/logout
+
+router.get('/logout', (req,res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const{ ensureAuth, ensureGuest} = require('../middleware/auth')
 
 //@desc Loogin/Landing Page
 //@route GET/
-router.get('/',  (req, res) => {
+router.get('/', ensureGuest,   (req, res) => {
     res.render('login', {
         layout:'login',
     })
@@ -17,8 +18,10 @@ router.get('/',  (req, res) => {
 
 //@desc Dashbaord
 //@route GET/dashboard
-router.get('/dashboard', (req,res)=>{
+router.get('/dashboard', ensureAuth, (req,res)=>{
+    console.log(req.user)
     res.render('dashboard')
+
 })
 
 // router.get("/dashboard",function(req,res){
