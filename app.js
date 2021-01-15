02cf8a33aeeb507bd6a2bd0d20ad0f7f91973f64
LettuceFoot <febrian.francione@gmail.com>
2021-01-15 09:38:48 -0500

    added styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // console.log("Hello world")
-
+const path= require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -19,6 +19,9 @@ if(process.env.NODE_ENV == 'development'){
 //handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//static
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,27 @@ const router = express.Router()
 
 //@desc Loogin/Landing Page
 //@route GET/
-// router.get('/',  (req, res) => {
-//     res.send('login')
-// })
+router.get('/',  (req, res) => {
+    res.render('login', {
+        layout:'login',
+    })
+})
 
-router.get("/",function(req,res){
-    // res.send('login');
-    res.render("login");
-
-});
+// router.get("/",function(req,res){
+//    j
+//     res.render("login");
+//
+// });
 
 //@desc Dashbaord
 //@route GET/dashboard
-// router.get('/dashboard', (req,res)=>{
-//     res.send('dashboard')
-// })
+router.get('/dashboard', (req,res)=>{
+    res.render('dashboard')
+})
 
-router.get("/dashboard",function(req,res){
-    res.send("dashboard");
-
-});
+// router.get("/dashboard",function(req,res){
+//     res.render("dashboard");
+//
+// });
 
 module.exports = router
